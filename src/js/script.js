6f88bc2edc8caddf69523831d0eaa0ed60ca209f
AlimaKarimova <way.s@inbox.ru>
2022-07-15 10:55:17 +0400

    slick slayder

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
    $('.carousel__inner').slick({
       speed: 1200,
-      adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-rightsolid.png"></button>'
+      prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left_arrow.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="/icons/right_arrow.svg"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               dots: true,
+               arrows:false
+            }
+         }
+      ]
    })
 })
